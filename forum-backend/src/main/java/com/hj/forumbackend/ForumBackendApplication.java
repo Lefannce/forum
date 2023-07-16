96eb10902c4fc52d0147277022a9216210b9f9e7
Tarma <hujing316@aliyun.com>
2023-07-16 15:00:20 +0800

    Initial commit

---
+++ forum-backend/src/main/java/com/hj/forumbackend/ForumBackendApplication.java
@@ -0,0 +1,13 @@
+package com.hj.forumbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForumBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForumBackendApplication.class, args);
+    }
+
+}
